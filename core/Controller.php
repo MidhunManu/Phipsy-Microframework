6cb13a99c555af2156dab5983656a1441d163e3c
MidhunManu <midhunmanu384@gmail.com>
2025-11-15 23:42:37 +0530

    refactor: remove redundant comments

--- core/Controller.php
+++ core/Controller.php
@@ -1,18 +1,5 @@
 <?php
 
-// namespace Core;
-
-// use Core\View;
-
-// class Controller
-// {
-//     public function view(string $name, array $data = [])
-//     {
-//         return View::make($name, $data);
-//     }
-// }
-
-
 namespace Core;
 
 class Controller
